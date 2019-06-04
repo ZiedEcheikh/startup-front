b12e23981e3a1c0fd6bcd64ada075fa91618bbf8
Zied ECHEIKH <zied.echeikh@mymoneybank.com>
2019-06-04 13:30:24 +0200

    Ãrouting modules

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HowitworkComponent } from './howitwork/howitwork.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { HomeComponent } from './home.component';
 const routes: Routes = [
-  { path: 'welcome', component: WelcomeComponent },
-  { path: 'howitwork', component: HowitworkComponent },
-  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'howitwork', component: HowitworkComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]
+  }
 ];
 
 @NgModule({
